4d5ebe831a33e16abf2bd36ea5c6516dcc69a4b1
hnsCreations <harin.ncsl@gmail.com>
2017-10-26 19:10:35 +0530

    new

---
+++ application/views/register.php
@@ -0,0 +1,101 @@
+<?php include 'partials/header.php'?>
+<!--banner-->
+<div class="banner-top">
+	<div class="container">
+		<h1>Register</h1>
+		<em></em>
+		<h2><a href="index.php">Home</a><label>/</label>Register</h2>
+	</div>
+</div>
+<!--login-->
+    <div class="container">
+        <div class="login">
+            <form>
+                <div class="col-md-6 login-do">
+                    <div class="login-mail">
+                        <input type="text" placeholder="Nic" required="">
+                        <i  class="fa fa-address-card" aria-hidden="true"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="text" placeholder="Name" required="">
+                        <i  class="glyphicon glyphicon-user"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="text" placeholder="Address line 1" required="">
+                        <i  class="fa fa-address-book-o" aria-hidden="true"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="text" placeholder="Address line 2" required="">
+                        <i  class="fa fa-address-book-o" aria-hidden="true"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="text" placeholder="Address line 3" required="">
+                        <i  class="fa fa-address-book-o" aria-hidden="true"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="text" placeholder="Mobile" required="">
+                        <i  class="fa fa-mobile" aria-hidden="true"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="text" placeholder="Telephone" required="">
+                        <i  class="fa fa-phone" aria-hidden="true"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="text" placeholder="Email" required="">
+                        <i  class="glyphicon glyphicon-envelope"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="password" placeholder="Password" required="">
+                        <i class="glyphicon glyphicon-lock"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="password" placeholder="Confirm Password" required="">
+                        <i class="glyphicon glyphicon-lock"></i>
+                    </div>
+                    <!--<a class="news-letter " href="#">
+                          <label class="checkbox1"><input type="checkbox" name="checkbox" ><i> </i>Forget Password</label>
+                        </a>-->
+                    <label class="hvr-skew-backward">
+                        <input type="submit" value="Submit">
+                    </label>
+
+                </div>
+                <div class="col-md-6 login-right">
+                    <h3>Keep in touch</h3>
+
+                    <p>Come join with us and be one of our valueble customers and get valuable deals from us.</p>
+
+                    <a href="login.html" class="hvr-skew-backward">Login</a>
+
+                </div>
+
+                <div class="clearfix"> </div>
+            </form>
+        </div>
+
+    </div>
+
+<!--//login-->
+
+		<!--brand-->
+		<div class="container">
+			<div class="brand">
+				<div class="col-md-3 brand-grid">
+					<img src="images/ic.png" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-3 brand-grid">
+					<img src="images/ic1.png" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-3 brand-grid">
+					<img src="images/ic2.png" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-3 brand-grid">
+					<img src="images/ic3.png" class="img-responsive" alt="">
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			</div>
+			<!--//brand-->
+	
+	<!--//content-->
+<?php include 'partials/footer.php'?>
